0f21dd5314428ce4bd17c13ff3343a9d6b5f5119
Antonio González Artime <antoniogartime@gmail.com>
2014-04-12 04:32:11 -0700

    Added ClickStats Sample Storm

---
+++ storm/ClickStats/src/main/java/com/agartime/utad/storm/Fields.java
@@ -0,0 +1,15 @@
+package com.agartime.utad.storm;
+
+public class Fields {
+	public static final String IP = "ip";
+	public static final String URL = "url";
+	public static final String CLIENT_KEY = "clientKey";
+	public static final String COUNTRY = "country";
+	public static final String COUNTRY_NAME = "country_name";
+	public static final String CITY = "city";
+	public static final String UNIQUE = "unique";
+	public static final String COUNTRY_TOTAL = "countryTotal";
+	public static final String CITY_TOTAL = "cityTotal";
+	public static final String TOTAL_COUNT = "totalCount";
+	public static final String TOTAL_UNIQUE = "totalUnique";
+}
---
+++ storm/ClickStats/src/main/java/com/agartime/utad/storm/IPResolver.java
@@ -0,0 +1,7 @@
+package com.agartime.utad.storm;
+
+import org.json.simple.JSONObject;
+
+public interface IPResolver {
+	public JSONObject resolveIP(String ip);
+}
